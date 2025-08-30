a42b3566884e9c8d54a0596cc66ca16278082e09
Arun Subramanian <arun.a.s006@gmail.com>
2025-08-30 15:27:54 +0530

    requirement.txt intialized with version and code to find version is also added

---
+++ version.py
@@ -0,0 +1,27 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "python-dotenv",
+    "ipykernel",
+    "langchain-groq",
+    "langchain-google-genai",
+    "langchain-community",
+    "faiss-cpu",
+    "structlog",
+    "PyMuPDF",
+    "pylint",
+    "langchain-core",
+    "pytest",
+    "streamlit",
+    "fastapi",
+    "uvicorn",
+    "python-multipart",
+    "docx2txt",
+    "pypdf"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")
